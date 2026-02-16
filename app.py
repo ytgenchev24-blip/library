59183ea02ec3497479de50d9e11e89f28a3e0d30
ytgenchev24-blip <ytgenchev24@gmail.com>
2026-02-16 12:15:01 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import st as streamlit
+
+books = [
+  "Chervenata shapchica",
+  "Kumcho vulcho i kuma lisa",
+  "Tom and Jerry",
+  "Robinzon kruzo",
+  "Pitur pan"
+]
+
+st.title("Book checker app")
+st.write("Enter the book title to check if it exists in the database.")
+
+user_input = st.text_input("Book title")
+
+if st.button("Check Book"):
+  if user_input.strip() == "":
+    st.warning("Please enter a book title.")
+  elif user_input in books:
+    st.sucees("The book exists in the database!")
+  else:
+    st.error("The book is NOT exists in the database")
+    
+
+  
+  
